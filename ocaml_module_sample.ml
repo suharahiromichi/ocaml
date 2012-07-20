f1a02fec0f205849572b0cfd647b14baa9d34db3
SUHARA Hiromichi <suharahiromichi@gmail.com>
2012-07-21 00:26:29 +0900

    first commit

---
+++ ocaml_module_sample.ml
@@ -0,0 +1,67 @@
+(* OCamlのモジュールのサンプル *)
+
+(* one.ml *)
+(* ファイルをひとつにまとめる場合
+   module Name : sig ... end = struct ... end;;
+   シグネチャが要らない場合は、module Name = struct ... end;; *)
+module Pool :
+sig
+  exception Empty
+  type 'a stack = SNil | SCell of 'a
+  val empty : 'a stack
+  val set : 'a -> 'a stack
+  val get : 'a stack -> 'a
+end = struct
+  exception Empty
+  type 'a stack = SNil | SCell of 'a
+  let empty = SNil
+  let set x = SCell x
+  let get = function 
+    | SNil -> raise Empty
+    | SCell x -> x
+end;;
+let s1 = Pool.set 1;;
+let s2 = Pool.set 2;;
+open Pool;;                             (* openはできる *)
+let a3 = get s2;;
+Printf.printf "%d\n" a3;;
+(* end *)
+
+
+(* ファイルを分割するとき。
+   mliファイルつまりシグネチャは必須。 *)
+(* ocamlopt pool.mli pool.ml main.ml -o test *)
+(* --------------------------------------------- *)
+(* pool.mli *)
+(* module Name : sig ... end;; *)
+module Pool :
+sig
+  exception Empty
+  type 'a pool = SNil | SCell of 'a
+  val empty : 'a pool
+  val set : 'a -> 'a pool
+  val get : 'a pool -> 'a
+end;;
+(* end *)
+(* --------------------------------------------- *)
+(* pool.ml *)
+(* module Name = struct ... end;; *)
+module Pool = struct
+  exception Empty
+  type 'a pool = SNil | SCell of 'a
+  let empty = SNil
+  let set x = SCell x
+  let get = function 
+    | SNil -> raise Empty
+    | SCell x -> x
+end;;
+(* end *)
+(* --------------------------------------------- *)
+(* main.ml *)
+include Pool;;
+let s1 = Pool.set 1;;
+let s2 = Pool.set 2;;
+open Pool;;                             (* openはできる *)
+let a3 = get s2;;
+Printf.printf "%d\n" a3;;
+(* end *)
